0d2d8b25c7c525e54fd95f74ff1a868609426af7
Shirrah <oraisharrish028@gmail.com>
2024-11-18 12:40:35 +0800

    1.10.0

--- php/default.php
+++ php/default.php
@@ -7,7 +7,7 @@ session_destroy();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
     <title>EFMS</title>
     <link rel="stylesheet" href="stylesheet/default.css">
 </head>
@@ -16,7 +16,7 @@ session_destroy();
         <div class="default-content">
             <div class="default-content-display">
                 <span class="default-ccs">College of Computer Studies</span>
-                <img src=".//.//assets/images/ccslogo.png" alt="CCS Logo" class="ccslogo">
+                <img src=".//.//assets/images/ccslogo.png" alt="Logo of College of Computer Studies" class="ccslogo" loading="lazy">
                 <span class="web-name">Event & Fee Management System (EFMS)</span>
             </div>
         </div>
