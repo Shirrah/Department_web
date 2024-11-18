24c6d5dbb6594347f7c530b4734b5faf94038844
Shirrah <oraisharrish028@gmail.com>
2024-11-18 12:06:21 +0800

    1.9.1

--- php/default.php
+++ php/default.php
@@ -3,15 +3,23 @@ session_unset();
 session_destroy();
 ?>
 
-
-<link rel="stylesheet" href="stylesheet/default.css">
-
-<div class="default_body">
-    <div class="default-content">
-        <div class="default-content-display">
-            <span class="default-ccs">College of Computer Studies</span>
-            <img src=".//.//assets/images/ccslogo.png" alt="" class="ccslogo">
-            <span class="web-name">Event & Fee Management System (EFMS)</span>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EFMS</title>
+    <link rel="stylesheet" href="stylesheet/default.css">
+</head>
+<body>
+    <div class="default_body">
+        <div class="default-content">
+            <div class="default-content-display">
+                <span class="default-ccs">College of Computer Studies</span>
+                <img src=".//.//assets/images/ccslogo.png" alt="CCS Logo" class="ccslogo">
+                <span class="web-name">Event & Fee Management System (EFMS)</span>
+            </div>
         </div>
     </div>
-</div>
+</body>
+</html>
