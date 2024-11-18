ab124d6773e5ec451bcf473e9a270819bbbb9eaa
Shirrah <oraisharrish028@gmail.com>
2024-11-18 13:09:49 +0800

    1.10.2

--- php/default.php
+++ php/default.php
@@ -3,12 +3,7 @@ session_unset();
 session_destroy();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
-    <title>EFMS</title>
     <link rel="stylesheet" href="stylesheet/default.css">
 </head>
 <body>
