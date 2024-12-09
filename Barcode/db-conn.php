bc7bab7302691ee2c68b0d79e5fd1b955c93989b
Shirrah <oraisharrish028@gmail.com>
2024-12-09 21:56:30 +0800

    db-conn 2

--- Barcode/db-conn.php
+++ Barcode/db-conn.php
@@ -1,15 +1,15 @@
 <?php
 class Database {
-    public $db; // Change the visibility to public
+    public $conn; // Change the visibility to public
     public $error; // Added property to store connection errors
 
     public function __construct() {
         // Establishing a connection to the database
-        $this->db = new mysqli("auth-db1632.hstgr.io", "u958767601_shirrah", "Shirrah612345", "u958767601_dcs");
+        $this->conn = new mysqli("auth-db1632.hstgr.io", "u958767601_shirrah", "Shirrah612345", "u958767601_dcs");
 
         // Checking for connection errors
-        if ($this->db->connect_error) {
-            $this->error = "Connection failed: " . $this->db->connect_error;
+        if ($this->conn->connect_error) {
+            $this->error = "Connection failed: " . $this->conn->connect_error;
             die($this->error); // Terminating script execution if connection fails
         }
     }
