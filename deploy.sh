0040bb93dc33efcd8de6765f81a204bd4278b497
Shirrah <oraisharrish028@gmail.com>
2025-03-27 02:42:15 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-cd /home/hpo-admin/htdocs/Department_web
-git pull origin main
+cd /home/hpo-admin/htdocs/Department_web  # Change to your project directory
+git reset --hard origin/main  # Ensure no local changes interfere
+git pull origin main  # Pull latest changes
+chown -R www-data:www-data /home/hpo-admin/htdocs/Department_web  # Adjust file permissions
+chmod -R 775 /home/hpo-admin/htdocs/Department_web  # Ensure correct permissions
