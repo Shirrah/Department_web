71e6ef935ef58ed4d553af6ad12ee4d298630bf2
Shirrah <oraisharrish028@gmail.com>
2025-04-21 19:02:57 +0800

    upd

--- php/admin/adminindex.php
+++ php/admin/adminindex.php
@@ -31,7 +31,10 @@ if (isset($_SESSION['user_data'])) {
     <title>Document</title>
 </head>
 <body>
-<link rel="stylesheet" href="http://localhost/Department_web/stylesheet/admin/admin-index.css">
+
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
 
 <style>
