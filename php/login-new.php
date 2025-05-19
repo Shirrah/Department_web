0e0a58c7138c3984aa9bfe63c834507cd8733e82
Shirrah <oraisharrish028@gmail.com>
2025-05-19 10:52:59 +0800

    Remove unused Font Awesome and Bootstrap links from login-new.php to streamline the code and reduce external dependencies.

--- php/login-new.php
+++ php/login-new.php
@@ -2,10 +2,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="stylesheet/login-new-main.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 
 
 <?php
